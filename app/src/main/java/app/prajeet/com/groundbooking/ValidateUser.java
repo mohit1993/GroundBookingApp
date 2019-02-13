dd83c0fc728687e11862aaafd6affab144ff09e1
Mohit Chauhan <shant.mohit1993@gmail.com>
2019-02-13 23:47:53 +0530

    adding Asnc Job to verify user

---
+++ app/src/main/java/app/prajeet/com/groundbooking/ValidateUser.java
@@ -0,0 +1,115 @@
+package app.prajeet.com.groundbooking;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class ValidateUser extends AsyncTask<String,String,String> {
+    Context context;
+    AlertDialog alertDialog;
+
+    public ValidateUser(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        Log.d("Mohit","doInBackground");
+        String type = params[0];
+        String login_url = "http://192.168.0.103:80/login.php";
+        if(type.equals("login")) {
+            try {
+                String phoneOrEmail = params[1];
+                String pass = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data = URLEncoder.encode("phoneOrEmail","UTF-8")+"="+URLEncoder.encode(phoneOrEmail,"UTF-8")+"&"
+                        +URLEncoder.encode("pass","UTF-8")+"="+URLEncoder.encode(pass,"UTF-8");
+                bufferedWriter.write(post_data);
+                Log.d("Mohit","Data posted");
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!=null) {
+                    result += line;
+                }
+                Log.d("Mohit",result);
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Runs on the UI thread before {@link #doInBackground}.
+     *
+     * @see #onPostExecute
+     * @see #doInBackground
+     */
+    @Override
+    protected void onPreExecute() {
+        Log.d("Mohit","PreExecute");
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    /**
+     * <p>Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     *
+     * <p>This method won't be invoked if the task was cancelled.</p>
+     *
+     * @param aVoid The result of the operation computed by {@link #doInBackground}.
+     * @see #onPreExecute
+     * @see #doInBackground
+     * @see #onCancelled(Object)
+     */
+    @Override
+    protected void onPostExecute(String  result) {
+        Log.d("Mohit","PostExecute");
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    /**
+     * Runs on the UI thread after {@link #publishProgress} is invoked.
+     * The specified values are the values passed to {@link #publishProgress}.
+     *
+     * @param values The values indicating progress.
+     * @see #publishProgress
+     * @see #doInBackground
+     */
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+}
